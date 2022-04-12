fcd9ed6a55057a04498b4c00d4fbe2bf64cdab68
Ayush Shrivastava <ayushshrivastava600@gmail.com>
2022-04-12 09:25:20 +0530

    update

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,41 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class movement : MonoBehaviour
 {
    [SerializeField]private float _speed = 7f;
     [SerializeField]private float _mouseSensitivity = 50f;
     [SerializeField]private float _minCameraview = -70f, _maxCameraview = 80f;
     private CharacterController _charController;
-    private Camera _camera;
+    [SerializeField] private Camera _fppcamera;
+        [SerializeField] private Camera _tppcamera;
+        private Camera _camera =null;
+        bool istpp;
+          [SerializeField] private GameObject _tppc;
+                    [SerializeField] private GameObject _fppc;
+
+
+
     private float xRotation = 0f;
-    [SerializeField]private Camera fpp;
+         Vector3 moveVector;
+         private bool iswalking;
+ private Animator animator;
+  float vertical,horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
         _charController = GetComponent<CharacterController>();
-        _camera = fpp;
+        _fppc.SetActive(true);
+                _tppc.SetActive(false);
+
+       _camera = _fppcamera;
+
+
+       istpp=false;
 
         if(_charController == null)
         Debug.Log("No Character Controller Attached to Player");
 
         Cursor.lockState = CursorLockMode.Locked;
+        animator= GetComponent<Animator>();
+        iswalking=false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Get WASD Input for Player
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+       // _camera.SetActive(true);
+         vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
         //move player based on WASD Input
         Vector3 movement = transform.forward * vertical + transform.right * horizontal; //changed this line.
         _charController.Move(movement * Time.deltaTime * _speed);
 
-
-        
+       
         //Get Mouse position Input
         float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity; //changed this line.
         float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity; //changed this line.
@@ -47,6 +68,55 @@ public class movement : MonoBehaviour
           _camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
           //Rotate the player based on the X input of the mouse
           transform.Rotate(Vector3.up * mouseX * 3);
+            
+            moveVector = Vector3.zero;
+ 
+         //Check if cjharacter is grounded
+         if (_charController.isGrounded == false)
+         {
+             //Add our gravity Vector
+             moveVector += Physics.gravity;
+         }
+ 
+         //Apply our move Vector , remeber to multiply by Time.delta
+         _charController.Move(moveVector * Time.deltaTime);
 
+
+         
+        //float horizontal = Input.GetAxis("Horizontal");
+            if(Input.GetKeyDown(KeyCode.C)){
+            if(istpp==false){
+            _camera=_tppcamera;
+            _fppc.SetActive(false);
+                _tppc.SetActive(true);
+            istpp=true;
+        }
+        else if(istpp==true){
+            _camera=_fppcamera;
+            _fppc.SetActive(true);
+                _tppc.SetActive(false);
+            istpp=false;
+        }
+        
+        }
+        
+        }
+          void FixedUpdate() {
+             // _camera.SetActive(true);
+               if(vertical>0||vertical<0||horizontal>0||horizontal<0){
+                         animator.SetBool("isWalking",true);
+              
+
+
+            }
+            else if(horizontal==0&&vertical==0){
+             animator.SetBool("isWalking",false);
+
+            }
+            else{
+                  animator.SetBool("isWalking",false);
+            }
+        
     }
+    
 }
